21be7cad6c8934c6b0c8bb840903a678eabc7670
hp <emmanueldusengimana83@gmail.com>
2023-08-04 17:01:27 +0200

    add

---
+++ js/login.js
@@ -0,0 +1,20 @@
+let username=document.getElementById("username")
+let email=document.getElementById("email")
+let password=document.getElementById("password")
+let form=document.querySelector("form")
+
+let users=JSON.parse(localStorage.getItem("users")) || []
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+
+    const userExist=users.find((user)=>user.email==email.value)
+    if(userExist && userExist.password==password.value){
+        window.location.href="../"
+    }else if(userExist && userExist.password!=password.value){
+        alert("Wrong password")
+    }else{
+        alert("user not exist signup first")
+        window.Location.href=""
+    }
+})
