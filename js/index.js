eb3a21596533227dd76518c058d078e7f01bff87
hp <emmanueldusengimana83@gmail.com>
2023-11-09 10:38:18 +0200

    huh

--- js/index.js
+++ js/index.js
@@ -1,10 +1,4 @@
-let form=document.querySelector("form")
-let fullname=document
 
-    form.addEventListener("submit",(e)=>{
-        e.preventDefault()
-
-    })
-     users.forEach((user)=>{
-        form.innerHTML+=`<textarea>${user.message}</textarea>`
-     })
+function showNav(){
+    const biraje=document.querySelector(".nav-icon").style.display="block"
+}
